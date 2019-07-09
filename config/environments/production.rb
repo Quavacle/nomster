920c36f173e8cd74d8b32616466569ea0a773ff8
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-07-09 17:32:31 +0000

    Added action mailer in, updated url to include https indicator

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.action.mailer.default_url_options = { host: 'https://nomster-bom.herokuapp.com/' }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
