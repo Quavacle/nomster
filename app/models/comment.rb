b211cc69b4a04f1f870e29efa01970028fe9262d
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-07-12 20:15:50 +0000

    Rating dropdown added, moved comment to modal

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,16 @@ class Comment < ApplicationRecord
     belongs_to :place
     validates :message, presence: true
     validates :rating, presence: true
+
+    RATINGS = {
+        'one star': '1_star',
+        'two star': '2_stars',
+        'three stars': '3_stars',
+        'four stars': '4_stars',
+        'five stars': '5_stars'
+    }
+
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end
