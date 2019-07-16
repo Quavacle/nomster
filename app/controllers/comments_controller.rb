69f6fd7790e99614eff5aee162888f9960c63bdb
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-07-16 08:00:34 +0000

    1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
         @place = Place.find(params[:place_id])
         ##@comment =## 
         @place.comments.create(comment_params.merge(user: current_user))
-        #if @comment.valid?
-         #   redirect_to place_path(@place)
+        #@comment.valid?
+        redirect_to place_path(@place)
         #else
         #    render :new, status: :unprocessable_entity
         #end
