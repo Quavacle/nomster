011cc11a71318a1dccb4fb290f37535826a19bf0
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-07-16 07:49:29 +0000

    removed instance variables to attempt creates to work again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
     before_action :authenticate_user!
     def create
         @place = Place.find(params[:place_id])
-        @comment = @place.comments.create(comment_params.merge(user: current_user))
-        if @comment.valid?
-            redirect_to place_path(@place)
-        else
-            render :new, status: :unprocessable_entity
-        end
+        ##@comment =## 
+        @place.comments.create(comment_params.merge(user: current_user))
+        #if @comment.valid?
+         #   redirect_to place_path(@place)
+        #else
+        #    render :new, status: :unprocessable_entity
+        #end
     end
 
     private
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,10 @@
 class PhotosController < ApplicationController
  before_action :authenticate_user!
+
  def create
     @place = Place.find(params[:place_id])
-    @pic = @place.photos.create(photo_params)
-    
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
-    
 end
 
 private
