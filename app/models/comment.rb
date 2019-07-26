4b3ad7fc89486f28070ac221145e57f10ee0de95
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-07-26 18:47:44 +0000

    Sentgrid for production mailer added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ApplicationRecord
     belongs_to :place
     validates :message, presence: true
     validates :rating, presence: true
-
+    after_create :send_comment_email
+    
     RATINGS = {
         'one star': '1_star',
         'two star': '2_stars',
@@ -15,5 +16,8 @@ class Comment < ApplicationRecord
     def humanized_rating
         RATINGS.invert[self.rating]
     end
-    
+
+    def send_comment_email
+        NotificationMailer.comment_added(self).deliver_now
+    end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,15 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.action_mailer.default_url_options = { host: 'https://nomster-bom.herokuapp.com/' }
-
+    ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
